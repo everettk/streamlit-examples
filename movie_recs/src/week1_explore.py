b32f667ee4ccf87650d4498a1e6673594c59008d
nodira <nodira@streamlit.io>
2019-01-08 16:37:59 -0800

    removed TODOs from week1. more consistent formatting around sections

--- movie_recs/src/week1_explore.py
+++ movie_recs/src/week1_explore.py
@@ -3,7 +3,6 @@ import numpy as np
 import pandas as pd
 
 st.title('Exploring Data with Streamlit')
-
 st.write("""
 When working with a new dataset, it's important to explore and understand it.
 Here we walk you through how we do this with the MovieLens dataset. To follow
@@ -19,92 +18,94 @@ Streamlit runs on save. If this doesn't work, click on the report-pane and press
 'r' to rerun.
 """)
 
-def get_users():
-    user_cols = ['user_id','age','gender','occupation','zip_code']
-    return pd.read_csv('../data/ml-100k/u.user', sep='|', names=user_cols, encoding='latin-1')
-
-users = get_users()
-st.subheader('Raw Data: users')
-st.write(users)
-
-st.write("""
-Now that we see the Users table. There are natural questions we will
-want to ask, e.g.
-- how many users are there?
-- how old do the users tend to be?
-- what is the gender distribution of the users?
-- what is the occuptation distribution of the users?
-- where do the users live?
-""")
-
-st.info("""
-2. Next up, we learn how to build vega lite charts with Streamlit. Uncomment the
-next section see a few examples!
-""")
-# -----------------------------------------------------------------------------
-
-st.write("Let's start with the easiest question first.")
-st.subheader('How many users are there?')
-st.write('Number of users: %s' % len(users))
-
-st.subheader('How old do the users tend to be?')
-st.write("""
-To answer this question, let's build a bar chart representing the distribution
-of ages across the users. We can do this with a Vega Lite chart as follows.
-""")
-
-with st.echo():
-    st.vega_lite_chart(users, {
-        'mark': 'bar',
-        'encoding': {
-            'x': {'field': 'age', 'type': 'ordinal'},
-            'y': {'aggregate': 'count', 'type': 'quantitative'},
-        },
-    })
-
-st.write("""
-We can now do the same thing with gender and occupation.
-""")
-
-st.subheader('What is the gender distribution of the users?')
-st.vega_lite_chart(users, {
-    'mark': 'bar',
-    'encoding': {
-        'x': {'field': 'gender', 'type': 'ordinal'},
-        'y': {'aggregate': 'count', 'type': 'quantitative'},
-
-    },
-})
-
-st.subheader('What is the occupation distribution of the users?')
-st.vega_lite_chart(users, {
-    'mark': 'bar',
-    'encoding': {
-        'x': {'field': 'occupation', 'type': 'ordinal'},
-        'y': {'aggregate': 'count', 'type': 'quantitative'},
-
-    },
-})
-
-st.subheader('Where do the users live?')
-st.write("""
-To answer this question, we need to plot the users on a map. Streamlit has
-support for deck gl charts, but for that we need the latitude and longitude.
-We currently only have zipcodes. How do we convert?
-We found a [libary](https://pypi.org/project/uszipcode/) that can do this. Let's
-try it on a few examples.
-""")
-
-with st.echo():
-    from uszipcode import SearchEngine
-    search = SearchEngine(simple_zipcode=True)
-
-st.info("""
-3. Uncomment the next section to try out the by_zipcode() function on a specific
-zipcode. Replace 94612 with your own zipcode!
-""")
-# -----------------------------------------------------------------------------
-
+# def get_users():
+#     user_cols = ['user_id','age','gender','occupation','zip_code']
+#     return pd.read_csv('../data/ml-100k/u.user', sep='|', names=user_cols, encoding='latin-1')
+#
+# users = get_users()
+# st.subheader('Raw Data: users')
+# st.write(users)
+#
+# st.write("""
+# Now that we see the Users table. There are natural questions we will
+# want to ask, e.g.
+# - how many users are there?
+# - how old do the users tend to be?
+# - what is the gender distribution of the users?
+# - what is the occuptation distribution of the users?
+# - where do the users live?
+# """)
+#
+# st.info("""
+# 2. Next up, we learn how to build vega lite charts with Streamlit. Uncomment the
+# next section see a few examples!
+# """)
+#
+# # # -----------------------------------------------------------------------------
+#
+# st.write("Let's start with the easiest question first.")
+# st.subheader('How many users are there?')
+# st.write('Number of users: %s' % len(users))
+#
+# st.subheader('How old do the users tend to be?')
+# st.write("""
+# To answer this question, let's build a bar chart representing the distribution
+# of ages across the users. We can do this with a Vega Lite chart as follows.
+# """)
+#
+# with st.echo():
+#     st.vega_lite_chart(users, {
+#         'mark': 'bar',
+#         'encoding': {
+#             'x': {'field': 'age', 'type': 'ordinal'},
+#             'y': {'aggregate': 'count', 'type': 'quantitative'},
+#         },
+#     })
+#
+# st.write("""
+# We can now do the same thing with gender and occupation.
+# """)
+#
+# st.subheader('What is the gender distribution of the users?')
+# st.vega_lite_chart(users, {
+#     'mark': 'bar',
+#     'encoding': {
+#         'x': {'field': 'gender', 'type': 'ordinal'},
+#         'y': {'aggregate': 'count', 'type': 'quantitative'},
+#
+#     },
+# })
+#
+# st.subheader('What is the occupation distribution of the users?')
+# st.vega_lite_chart(users, {
+#     'mark': 'bar',
+#     'encoding': {
+#         'x': {'field': 'occupation', 'type': 'ordinal'},
+#         'y': {'aggregate': 'count', 'type': 'quantitative'},
+#
+#     },
+# })
+#
+# st.subheader('Where do the users live?')
+# st.write("""
+# To answer this question, we need to plot the users on a map. Streamlit has
+# support for deck gl charts, but for that we need the latitude and longitude.
+# We currently only have zipcodes. How do we convert?
+# We found a [libary](https://pypi.org/project/uszipcode/) that can do this. Let's
+# try it on a few examples.
+# """)
+#
+# with st.echo():
+#     from uszipcode import SearchEngine
+#     search = SearchEngine(simple_zipcode=True)
+#
+# st.info("""
+# 3. Uncomment the next section to try out the by_zipcode() function on a specific
+# zipcode. Replace 94612 with your own zipcode!
+# """)
+#
+# # # -----------------------------------------------------------------------------
+#
 # st.write("Let's try our SearchEngine on a specific zipcode.")
 # st.write(search.by_zipcode(94612))
 #
@@ -118,19 +119,20 @@ zipcode. Replace 94612 with your own zipcode!
 # and uncomment the next to see how we can add the necessary columns to our users
 # dataframe.
 # """)
-
-# -----------------------------------------------------------------------------
-
-users['lat'] =  users['zip_code'].apply(lambda z : search.by_zipcode(z).lat )
-users['lon'] =  users['zip_code'].apply(lambda z : search.by_zipcode(z).lng )
-st.write(users)
-
-st.info("""
-4. Great. Check out those beautiful lat & lon columns! Let's put this on a map.
-Uncomment the next section.
-""")
-# -----------------------------------------------------------------------------
-
+#
+# # # -----------------------------------------------------------------------------
+#
+# users['lat'] =  users['zip_code'].apply(lambda z : search.by_zipcode(z).lat )
+# users['lon'] =  users['zip_code'].apply(lambda z : search.by_zipcode(z).lng )
+# st.write(users)
+#
+# st.info("""
+# 4. Great. Check out those beautiful lat & lon columns! Let's put this on a map.
+# Uncomment the next section.
+# """)
+#
+# # # -----------------------------------------------------------------------------
+#
 # st.deck_gl_chart(
 #     layers=[{
 #         'type': 'ScatterplotLayer',
@@ -148,9 +150,9 @@ Uncomment the next section.
 # st.info("""
 # 4a. Comment out this section and uncomment the next section.
 # """)
-
-# -----------------------------------------------------------------------------
-
+#
+# # # -----------------------------------------------------------------------------
+#
 # st.write("""
 # We've now centered the map around the latitude and longitude of our first user.
 # However, we still can't see the points on the map.
@@ -172,114 +174,107 @@ Uncomment the next section.
 # 4b. Let's draw our points with a larger radius (so that we can see them).
 # Comment out this section and uncomment the next.
 # """)
-
-# -----------------------------------------------------------------------------
-
-st.deck_gl_chart(
-        viewport={
-            'latitude': 32,
-            'longitude': -110,
-            'zoom': 3
-        },
-        layers=[{
-            'type': 'ScatterplotLayer',
-            'data': users,
-            'radiusScale': 10,
-            'getRadius': 1000
-        }]
-    )
-
-st.info("""
-5. Almost done ... feel free to play with the viewport above to center the map
-a little better. Once you're done, uncomment the next section.
-""")
-# -----------------------------------------------------------------------------
-
-st.info("""
-6. Uncomment the remainder of this file to see how we explore the other parts
-of this dataset. Enjoy!
-""")
-
-# -----------------------------------------------------------------------------
-
-st.write("""
-There are many other ways in which we can explore the Users table.
-
-But for now, let's take a look at the other tables. We can follow a very
-similar exploration process for the Movies and Ratings tables too. We don't
-include explanations for these two tables in this report, but do check out
-the underlying code here.
-""")
-#TODO: add <here> link above
-
-
-st.header('Movies')
-movie_cols = ['movie_id','movie_title','release_date', 'video_release_date','IMDb_URL','unknown','Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War' ,'Western']
-movies = pd.read_csv('../data/ml-100k/u.item', sep='|', names=movie_cols, encoding='latin-1')
-
-st.subheader('Raw Data: movies')
-st.write(movies)
-
-st.subheader('How many movies are there?')
-st.write('Number of movies: %s' % len(movies))
-
-st.subheader('How does the genre breakdown look?')
-genre_cols = ['Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War' ,'Western']
-movie_genres = pd.DataFrame(movies, columns=genre_cols)
-
-genre_counts = movie_genres.sum()
-genre_counts = genre_counts.reset_index()
-genre_counts = genre_counts.rename({'index':'genre', 0:'count'}, axis=1)
-st.write(genre_counts)
-
-st.subheader('Genre Distribution')
-st.vega_lite_chart(genre_counts, {
-    'mark': 'bar',
-    'encoding': {
-        'x': {'field': 'genre', 'type': 'ordinal'},
-        'y': {'field': 'count', 'type': 'quantitative'},
-
-    },
-})
-
-st.header('Ratings')
-rating_cols = ['user_id', 'item_id', 'rating', 'timestamp']
-ratings = pd.read_csv('../data/ml-100k/u.data', sep='\t', names=rating_cols, encoding='latin-1')
-
-st.subheader('Raw Data: ratings')
-st.write(ratings)
-
-st.subheader('How many ratings are there?')
-st.write('Number of ratings: %s' % len(ratings))
-
-st.subheader('What is the distribution of ratings?')
-
-st.vega_lite_chart(ratings, {
-    'mark': 'bar',
-    'encoding': {
-        'x': {'field': 'rating', 'type': 'ordinal'},
-        'y': {'aggregate': 'count', 'type': 'quantitative'},
-
-    },
-})
-
-st.subheader('What is the distribution of ratings per user??')
-# TODO: sometimes stops here - bug report!
-#TODO: the history of this line
-#ratings_per_user = df_data.groupby(['user_id']).agg(['count'])
-
-ratings_per_user = pd.DataFrame(ratings.groupby(['user_id']).size())
-ratings_per_user = ratings_per_user.rename({0:'ratings_by_user'}, axis=1)
-st.write(ratings_per_user)
-st.vega_lite_chart(ratings_per_user, {
-    'mark': 'bar',
-    'encoding': {
-        'x': {'field': 'ratings_by_user', 'type': 'ordinal'},
-        'y': {'aggregate': 'count', 'type': 'quantitative'},
-    },
-})
-# TODO: clean up imports in each file
-# TODO: make sure it works with python 2.7
-
-st.info("Check out week2_rec_v0.py for the next part of this guide.")
-# TODO: link to the next part of the tutorial
+#
+# # # -----------------------------------------------------------------------------
+#
+# st.deck_gl_chart(
+#         viewport={
+#             'latitude': 32,
+#             'longitude': -110,
+#             'zoom': 3
+#         },
+#         layers=[{
+#             'type': 'ScatterplotLayer',
+#             'data': users,
+#             'radiusScale': 10,
+#             'getRadius': 1000
+#         }]
+#     )
+#
+# st.info("""
+# 5. Almost done ... feel free to play with the viewport above to center the map
+# a little better. Once you're done, uncomment the next section.
+# """)
+#
+# # # -----------------------------------------------------------------------------
+#
+# st.info("""
+# 6. Uncomment the remainder of this file to see how we explore the other parts
+# of this dataset. Enjoy!
+# """)
+#
+# # # -----------------------------------------------------------------------------
+#
+# st.write("""
+# There are many other ways in which we can explore the Users table.
+#
+# But for now, let's take a look at the other tables. We can follow a very
+# similar exploration process for the Movies and Ratings tables too. We don't
+# include explanations for these two tables in this report, but do check out
+# the underlying code here.
+# """)
+#
+#
+# st.header('Movies')
+# movie_cols = ['movie_id','movie_title','release_date', 'video_release_date','IMDb_URL','unknown','Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War' ,'Western']
+# movies = pd.read_csv('../data/ml-100k/u.item', sep='|', names=movie_cols, encoding='latin-1')
+#
+# st.subheader('Raw Data: movies')
+# st.write(movies)
+#
+# st.subheader('How many movies are there?')
+# st.write('Number of movies: %s' % len(movies))
+#
+# st.subheader('How does the genre breakdown look?')
+# genre_cols = ['Action','Adventure','Animation','Childrens','Comedy','Crime','Documentary','Drama','Fantasy','Film-Noir','Horror','Musical','Mystery','Romance','Sci-Fi','Thriller','War' ,'Western']
+# movie_genres = pd.DataFrame(movies, columns=genre_cols)
+#
+# genre_counts = movie_genres.sum()
+# genre_counts = genre_counts.reset_index()
+# genre_counts = genre_counts.rename({'index':'genre', 0:'count'}, axis=1)
+# st.write(genre_counts)
+#
+# st.subheader('Genre Distribution')
+# st.vega_lite_chart(genre_counts, {
+#     'mark': 'bar',
+#     'encoding': {
+#         'x': {'field': 'genre', 'type': 'ordinal'},
+#         'y': {'field': 'count', 'type': 'quantitative'},
+#
+#     },
+# })
+#
+# st.header('Ratings')
+# rating_cols = ['user_id', 'item_id', 'rating', 'timestamp']
+# ratings = pd.read_csv('../data/ml-100k/u.data', sep='\t', names=rating_cols, encoding='latin-1')
+#
+# st.subheader('Raw Data: ratings')
+# st.write(ratings)
+#
+# st.subheader('How many ratings are there?')
+# st.write('Number of ratings: %s' % len(ratings))
+#
+# st.subheader('What is the distribution of ratings?')
+#
+# st.vega_lite_chart(ratings, {
+#     'mark': 'bar',
+#     'encoding': {
+#         'x': {'field': 'rating', 'type': 'ordinal'},
+#         'y': {'aggregate': 'count', 'type': 'quantitative'},
+#
+#     },
+# })
+#
+# st.subheader('What is the distribution of ratings per user??')
+# ratings_per_user = pd.DataFrame(ratings.groupby(['user_id']).size())
+# ratings_per_user = ratings_per_user.rename({0:'ratings_by_user'}, axis=1)
+# st.write(ratings_per_user)
+# st.vega_lite_chart(ratings_per_user, {
+#     'mark': 'bar',
+#     'encoding': {
+#         'x': {'field': 'ratings_by_user', 'type': 'ordinal'},
+#         'y': {'aggregate': 'count', 'type': 'quantitative'},
+#     },
+# })
+#
+# st.info("Check out week2_rec_v0.py for the next part of this guide.")
